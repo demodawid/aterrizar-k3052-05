7edc193f4046bb0d1eefed6059f9db0b69156084
VickyUTN <gmariavictoria@hotmail.com>
2012-11-26 17:31:33 -0800

    homes y update

---
+++ src/homes/HomeAsiento.java
@@ -0,0 +1,68 @@
+package homes;
+
+import java.io.Serializable;
+import java.util.List;
+
+import aterrizar.AerolineaAdapter;
+import aterrizar.Asiento;
+import aterrizar.Usuario;
+
+import org.uqbar.commons.utils.Observable;
+
+import uqbar.arena.persistence.PersistentHome;
+
+@Observable
+public class HomeAsiento extends PersistentHome<Asiento> implements Serializable  {
+	
+	private static HomeAsiento instance;
+	
+	public static synchronized HomeAsiento getInstance() {
+		if (instance == null) {
+			instance = new HomeAsiento();
+		}
+		return instance;
+	}
+
+	private HomeAsiento() {
+	}
+	
+
+	///////VEERRR
+   // public void create(Asiento asiento) {
+//		super.create(asiento);
+	//}
+    
+    public List<Asiento> search(String fechaSal,int numero, String codigoDeVuelo, Float precio, String clase, String ubicacion,AerolineaAdapter aerolinea) {
+		Asiento example = new Asiento(fechaSal, numero, codigoDeVuelo, precio, clase, ubicacion, aerolinea);
+		return this.searchByExample(example);
+	}
+
+	@Override
+	public Class<Asiento> getEntityType() {
+		return Asiento.class;
+	}
+
+	@Override
+	public Asiento createExample() {
+		return new Asiento();
+	}
+    
+	
+	
+	public float comprar(Asiento asiento, Usuario usuario) throws Exception
+	{
+		asiento.getAerolinea().comprar(asiento, usuario);
+		float precioTotal = (asiento.getPrecio()*((asiento.getAerolinea().getPorcentajeImpuesto()/100) + 1)) + usuario.impuestoAdicional();
+	
+		return precioTotal;
+	}
+	
+	public void reservar(Asiento asiento, Usuario usuario) {
+		asiento.getAerolinea().reservar(asiento,usuario);
+	}
+
+	public void sobreReservar(Asiento asiento, Usuario usuario) {
+		asiento.getAerolinea().sobreReservar(asiento,usuario);		
+	}
+
+}
---
+++ src/homes/HomeUsuario.java
@@ -0,0 +1,48 @@
+package homes;
+
+/*import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import aterrizar.AerolineaAdapter;
+import aterrizar.Asiento;
+import aterrizar.Usuario;
+
+import org.uqbar.commons.utils.Observable;
+
+import uqbar.arena.persistence.PersistentHome;
+
+@Observable
+public class HomeUsuario extends PersistentHome<Usuario> implements Serializable  {
+	
+	private static HomeUsuario instance;
+	
+	public static synchronized HomeUsuario getInstance() {
+		if (instance == null) {
+			instance = new HomeUsuario();
+		}
+		return instance;
+	}
+
+	private HomeUsuario() {
+	}
+	
+
+	///////VEERRR
+   // public void create(Asiento asiento) {
+//		super.create(asiento);
+	//}
+    
+ 
+
+	@Override
+	public Class<Usuario> getEntityType() {
+		return Usuario.class;
+	}
+
+	@Override
+	public Usuario createExample() {
+		return ();
+	}
+
+}*/
